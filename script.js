597ade8233aa4727293efd3baef421e160b41a51
SKG-M <msdevelop221@gmail.com>
2020-03-27 10:15:36 +0900

    4.3th

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   const button = document.querySelector('button[data-action="change"]');
-  button.innerText = 'BUTTON';
+  button.innerText = '?';
   let places = staticLoadPlaces();
   renderPlaces(places);
 };
